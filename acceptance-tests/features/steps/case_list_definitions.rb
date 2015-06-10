0941ce48774ccedbecddfa44bc8784bc8e19540d
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-06-10 16:51:46 +0100

    added tests for both us31 scenarios and find data in table helper function

--- acceptance-tests/features/steps/case_list_definitions.rb
+++ acceptance-tests/features/steps/case_list_definitions.rb
@@ -3,25 +3,33 @@ Given(/^I navigate to the case list page$/) do
 end
 
 Then(/^the case reference number is displayed$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.find(:css, '.casework-dashboard tr', :text => 'FGHIK987')
 end
 
 Then(/^the client is displayed$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  find_data_in_table_row('.casework-dashboard', 'FGHIK987', 'David Stephens')
+  find_data_in_table_row('.casework-dashboard', 'FGHIK987', 'Maria Stephens')
 end
 
 Then(/^the task is displayed$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  find_data_in_table_row('.casework-dashboard', 'FGHIK987', 'Remortgage')
 end
 
 Then(/^the status is displayed$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  find_data_in_table_row('.casework-dashboard', 'FGHIK987', 'Mortgage deed created')
 end
 
 Then(/^the last updated date is displayed$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  find_data_in_table_row('.casework-dashboard', 'FGHIK987', '2 March 2015')
 end
 
 Then(/^the case list is sorted by last updated date$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  ### Retrieve all dates in the table assuming that the dates
+  ### are contained in the fifth column of the table
+  dates = page.all(:css, '.casework-dashboard tr td:nth-child(5)')
+  ### Check each set of two consecutive dates are same or in descending order
+  datesDescending = dates.each_cons(2).all? { |firstDate, secondDate|
+    (Date.parse(firstDate.text) <=> Date.parse(secondDate.text)) >= 0
+  }
+  assert_equal(true, datesDescending)
 end
---
+++ acceptance-tests/features/support/functions.rb
@@ -0,0 +1,7 @@
+def find_data_in_table_row(tableID, rowID, dataValue)
+  ### Retrieve the contents of the specified row
+  row = page.find(:css, tableID + ' tr', :text => rowID).text
+  ### Check supplied data value is present in the table row and return result
+  assertion = row.include? dataValue
+  assert_equal(true, assertion)
+end
