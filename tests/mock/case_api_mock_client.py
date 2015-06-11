7eb9566a3f2baf888b5ba78a25ef7f684a49af84
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-06-11 09:59:43 +0100

    mocked json response in unit tests

--- tests/mock/case_api_mock_client.py
+++ tests/mock/case_api_mock_client.py
@@ -1,4 +1,12 @@
+from tests.mock.mock_response import MockJsonResponse
+
+
 class CaseApiMockClient:
     @staticmethod
     def get_cases():
-        return {}
+        cases = [{"reference": "1000",
+                  "clients": ["John Smith", "Alice Young"],
+                  "task": "Remortgage",
+                  "status": "Draft",
+                  "last_updated": "20 May 2014"}]
+        return MockJsonResponse(cases)
