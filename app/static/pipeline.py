47d518c58d6ce4511d9984bf484a19f7f9fc4599
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-06-12 14:50:44 +0100

    reverted

--- app/static/pipeline.py
+++ app/static/pipeline.py
@@ -5,14 +5,13 @@ import sass as libsass
 
 __dot = path.dirname(path.realpath(__file__))
 __toolkit_dir = path.join(__dot, 'govuk_frontend_toolkit/stylesheets/')
-__elemets_dir = path.join(__dot, 'sass/elements/')
 
 
 def __compile_sass(_in, out, **kw):
     out.write(
         libsass.compile(
             string=_in.read(),
-            include_paths=[__toolkit_dir, __elemets_dir]
+            include_paths=[__toolkit_dir]
         )
     )
 
