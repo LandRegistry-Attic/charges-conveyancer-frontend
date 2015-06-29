daba59ef1739ed2532941d7064d84f0fcab1a80b
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-06-29 18:59:35 +0100

    added function to create data using API

--- acceptance-tests/features/support/functions.rb
+++ acceptance-tests/features/support/functions.rb
@@ -5,3 +5,21 @@ def find_data_in_table_row(tableID, rowID, dataValue)
   assertion = row.include? dataValue
   assert_equal(true, assertion)
 end
+
+def create_case_data(case_file)
+  ### Read data from the supplied json filename
+  case_json = File.read(File.dirname(__FILE__) + '/../../data/' + case_file + '.json')
+  ### Setup a POST request to the case API to insert the data
+  url = URI.parse($CASE_API_URL + "/case")
+  connection = Net::HTTP.new(url.host, url.port)
+  request = Net::HTTP::Post.new(url, initheader = {
+    'Content-Type' => 'application/json'})
+  request.body = "#{case_json}"
+  ### Execute the created request against the case API and return the response
+  response = connection.request(request)
+  if response.code == "200"
+    JSON.parse(response.body)
+  else
+    raise "Failed to create case with json #{case_json}. Got response #{response.code} - #{response.body}"
+  end
+end
