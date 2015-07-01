e8f31943f7aded2d2eee78bb5594d11cee7bea95
endamccormack <endamccormack0@gmail.com>
2015-07-01 09:06:09 +0100

    added acceptance test that matches feed from case-api

--- acceptance-tests/features/support/functions.rb
+++ acceptance-tests/features/support/functions.rb
@@ -1,6 +1,6 @@
-def find_data_in_table_row(tableID, rowID, dataValue)
+def find_data_in_table_row(table_row_id, unique_value, dataValue)
   ### Retrieve the contents of the specified row
-  row = page.find(:css, tableID + ' tr', text: rowID).text
+  row = page.find(:css, table_row_id, text: unique_value).text
   ### Check supplied data value is present in the table row and return result
   assertion = row.include? dataValue
   assert_equal(true, assertion)
