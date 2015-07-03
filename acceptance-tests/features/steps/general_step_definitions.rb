135cf0abad668c40d313a7088564d5f50ff5ec19
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-07-03 13:38:56 +0100

    added feature for conveyancer and skeleton step defs

---
+++ acceptance-tests/features/steps/general_step_definitions.rb
@@ -0,0 +1,11 @@
+Given(/^I navigate to the conveyancer frontend "([^"]*)" page$/) do |path|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the "([^"]*)" button$/) do |button_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the "([^"]*)" page is displayed$/) do |page_title|
+  pending # Write code here that turns the phrase above into concrete actions
+end
