e3c8624947b48b694234142e776b36d97551b512
Michael Allen <michael@michaelallen.io>
2015-07-08 15:50:26 +0100

    my flake8 didn't pick that up

--- app/case/routes.py
+++ app/case/routes.py
@@ -7,7 +7,8 @@ def register_routes(blueprint, case_api):
     @blueprint.route('/cases')
     def case_list():
 
-        def last_updated(case): return case.last_updated
+        def last_updated(case):
+            return case.last_updated
 
         cases = case_api.get_cases()
         cases_sorted = sorted(cases, key=last_updated, reverse=True)
