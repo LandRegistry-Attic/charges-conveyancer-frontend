7230a99c168fef5f1bd969e5118a66f42be26208
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-07-28 15:06:40 +0100

    cleanup

--- acceptance-tests/features/support/functions.rb
+++ acceptance-tests/features/support/functions.rb
@@ -30,7 +30,7 @@ end
 def create_case_data
   case_json = {
     'deed_id' => @deed_id,
-    'conveyancer_id' =>ngem '1'
+    'conveyancer_id' => '1'
   }
   response = HTTP.post($CASE_API_URL + '/case', json: case_json)
   if response.code == 201
