6e0c351e144ef4cbbc8c822159b96efabc57119a
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2015-07-30 11:53:23 +0100

    updated config file for deed api

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 DEBUG = True
 CASE_API_BASE_HOST = os.getenv('CASE_API_BASE_HOST',
-                               'http://0.0.0.0:7000')
+                               'http://case-api.dev.service.gov.uk')
 DEED_API_BASE_HOST = os.getenv('DEED_API_BASE_HOST',
-                               'http://0.0.0.0:10000')
+                               'http://deedapi.dev.service.gov.uk')
