4305357286c5ba5efe168297ffcf2e8b5447eda5
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-08-14 09:46:20 +0100

    added step to cover whole add borrower function and tidied up general defs file

---
+++ acceptance-tests/features/steps/add_borrower_definitions.rb
@@ -0,0 +1,22 @@
+################################################################################
+### Below are staircase steps, that perform basic functions for this part of ###
+### the project but are wrapped up in a more meaningful and concise name     ###
+################################################################################
+
+When(/^I add the following borrower to a case:$/) do |borrowers|
+  borrowers.hashes.each do |borrower|
+    step %(I click on the "Add a borrower to this case" link)
+    step %(I enter "#{first name}" into the "first-name" field)
+    step %(I enter "#{middle name}" into the "middle-name" field)
+    step %(I enter "#{last name}" into the "last-name" field)
+    step %(I enter "#{address}" into the "address" field)
+    step %(I enter "#{mobile number}" into the "mobile-number" field)
+    step %(I enter "#{email address}" into the "email-address" field)
+    step %(I click on the "Add borrower to case" button)
+  end
+end
+
+################################################################################
+### Below are the main bulk of the step definitions for this part of the     ###
+### project                                                                  ###
+################################################################################
