2fcebcd83506766f0328557c8d0736d476871b4a
cs112mm <mandy.nimmo@digital.landregistry.gov.uk>
2015-08-19 15:40:25 +0100

    acceptance tests for US07

---
+++ acceptance-tests/features/steps/apply_to_register.rb
@@ -0,0 +1,32 @@
+Given(/^all borrowers have signed the deed$/) do
+    visit($BORROWER_FRONTEND_URL + '/deed/search')
+    @deed = HTTP.get($DEED_API_URL + '/deed/' + @deed_id.to_s)
+    @borrower_token =
+    JSON.parse(@deed.body)['deed']['operative-deed']['borrowers'][0]['token']
+    fill_in('borrower_token', with: @borrower_token)
+    click_button('Search')
+    click_button('Sign the deed')
+
+    visit($BORROWER_FRONTEND_URL + '/deed/search')
+    @deed = HTTP.get($DEED_API_URL + '/deed/' + @deed_id.to_s)
+    @borrower_token =
+    JSON.parse(@deed.body)['deed']['operative-deed']['borrowers'][1]['token']
+    fill_in('borrower_token', with: @borrower_token)
+    click_button('Search')
+    click_button('Sign the deed')
+end
+
+And(/^conveyancer has made the deed effective$/) do
+  step %(I navigate to the conveyancer frontend "/cases" page)
+  click_link('Confirm completion')
+  click_button('Confirm completion')
+end
+
+When(/^I select to Apply to register the deed$/) do
+  find(:xpath, ".//*[@id='casework-dashboard']/tbody/tr[1]/td[6]/a").click
+  click_button('Submit')
+end
+
+Then(/^the case status has changed to submitted$/) do
+  step %(the status for the case is "([^"]*)"$/)
+end
