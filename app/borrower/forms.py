f2cb72bae0fb5a046befab0b0d78fe03364f1f3c
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-08-25 13:41:40 +0100

    US14 - add borrowers - form validation improved

--- app/borrower/forms.py
+++ app/borrower/forms.py
@@ -3,10 +3,34 @@ from wtforms import StringField, validators
 
 
 class AddBorrower(Form):
-    first_name = StringField('first_name', [validators.DataRequired()])
-    last_name = StringField('last_name', [validators.DataRequired()])
-    mobile_no = StringField('mobile_no', [validators.DataRequired()])
-    email_address = StringField('email_address', [validators.DataRequired()])
-    locality = StringField('locality', [validators.DataRequired()])
-    postcode = StringField('postcode', [validators.DataRequired()])
-    street_address = StringField('street_address', [validators.DataRequired()])
+    first_name_msg = 'You have to enter First name'
+    first_name = StringField('first_name',
+                             [validators.DataRequired(first_name_msg)])
+    middle_names = StringField('middle_names')
+    last_name_msg = 'You have to enter Last name'
+    last_name = StringField('last_name',
+                            [validators.DataRequired(last_name_msg)])
+    mobile_no_msg = 'You have to enter Mobile number'
+    mobile_no = StringField('mobile_no',
+                            [validators.DataRequired(mobile_no_msg)])
+    email_address_msg = 'You have to enter correct Email address'
+    email_address = StringField('email_address',
+                                [validators.Email(email_address_msg)])
+    locality_msg = 'You have to enter Locality'
+    locality = StringField('locality',
+                           [validators.DataRequired(locality_msg)])
+    postcode_msg = 'You have to enter Postcode'
+    postcode = StringField('postcode',
+                           [validators.DataRequired(postcode_msg)])
+    street_address_msg = 'You have to enter Street address'
+    street_address = StringField('street_address',
+                                 [validators.DataRequired(street_address_msg)])
+    extended_address = StringField('extended_address')
+
+    def get_attribute(self, attribute_name):
+        if hasattr(self, attribute_name):
+            attribute = getattr(self, attribute_name)
+            data = getattr(attribute, 'data')
+            if data is not None:
+                return data
+        return ''
--- app/borrower/views.py
+++ app/borrower/views.py
@@ -7,6 +7,16 @@ class AddBorrower(Template, FormHandler):
     pageTitle = "Add a new borrower"
 
     def __init__(self, form, case_id):
-        self.create_case_url = url_for('case.case_details', case_id = case_id)
+        self.create_case_url = url_for('case.case_details', case_id=case_id)
         self.form = form
         self.has_errors = form.errors != {}
+
+        self.first_name = form.get_attribute('first_name')
+        self.last_name = form.get_attribute('last_name')
+        self.middle_names = form.get_attribute('middle_names')
+        self.mobile_no = form.get_attribute('mobile_no')
+        self.email_address = form.get_attribute('email_address')
+        self.locality = form.get_attribute('locality')
+        self.postcode = form.get_attribute('postcode')
+        self.street_address = form.get_attribute('street_address')
+        self.extended_address = form.get_attribute('extended_address')
