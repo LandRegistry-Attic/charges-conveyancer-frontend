36817144a684bd8ae906f548b4d969c8791b396f
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-09-02 16:27:20 +0100

    US22 - removed unnecessary code

--- app/property/forms.py
+++ app/property/forms.py
@@ -7,10 +7,3 @@ class AddProperty(Form):
     title_number = StringField('title_number',
                                [validators.DataRequired(title_number_msg)])
 
-    def get_attribute(self, attribute_name):
-        if hasattr(self, attribute_name):
-            attribute = getattr(self, attribute_name)
-            data = getattr(attribute, 'data')
-            if data is not None:
-                return data
-        return ''
